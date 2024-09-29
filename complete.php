5f57af7583d4f2f1f8e99cf85b771978ae26c4a8
ponpokoyokoko <hyyh1025@gmail.com>
2024-09-29 11:04:46 +0900

    Add kadai_web_app files

---
+++ complete.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <!-- <h2><?php echo $name; ?>様、お問い合わせを承りました。</h2> -->
+    <h2>登録が完了しました。</h2>
+    <p>社員情報がデータベースに保存されました。</p>
+    <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+
+
+
+  </body>
+
+</html>
